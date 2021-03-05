5af8e93aedaf6158b1f52d73399ae96d49c97cb6
= <=>
2021-03-05 16:08:03 +0500

    Updates

---
+++ precache-manifest.a7c942ab8448b1ca267a1ec7c5f20386.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68247eb27e100eae82cfc6bcce592943",
+    "url": "/store-recording/index.html"
+  },
+  {
+    "revision": "9afdb92564b10af3adb5",
+    "url": "/store-recording/static/css/2.25d2dfdd.chunk.css"
+  },
+  {
+    "revision": "9b0818f54b51f54bb716",
+    "url": "/store-recording/static/css/main.024d2143.chunk.css"
+  },
+  {
+    "revision": "9afdb92564b10af3adb5",
+    "url": "/store-recording/static/js/2.91997c70.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/store-recording/static/js/2.91997c70.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9b0818f54b51f54bb716",
+    "url": "/store-recording/static/js/main.1b2189fa.chunk.js"
+  },
+  {
+    "revision": "f8b95e414877c4097c44",
+    "url": "/store-recording/static/js/runtime-main.fcf664e1.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/store-recording/static/media/logo.9f7e506d.svg"
+  },
+  {
+    "revision": "d63d9eff35aa057bab9c5041938d6d69",
+    "url": "/store-recording/static/media/product-1.d63d9eff.PNG"
+  },
+  {
+    "revision": "ad7ef891046b2a56aa41db25cbcf2a9b",
+    "url": "/store-recording/static/media/product-2.ad7ef891.PNG"
+  },
+  {
+    "revision": "d27ec2bc66073aa4b0b80368a9ac410e",
+    "url": "/store-recording/static/media/product-3.d27ec2bc.PNG"
+  },
+  {
+    "revision": "d9ed75372e686c2f8c38a53a52482e66",
+    "url": "/store-recording/static/media/product-4.d9ed7537.PNG"
+  },
+  {
+    "revision": "8f9a1f89508713ff90dc1b497c92b1b2",
+    "url": "/store-recording/static/media/product-5.8f9a1f89.PNG"
+  },
+  {
+    "revision": "b49d1b7cf1b82dad0a8059302706685a",
+    "url": "/store-recording/static/media/product-6.b49d1b7c.PNG"
+  },
+  {
+    "revision": "4c6ab223c32bec22d9cd6070caaedf19",
+    "url": "/store-recording/static/media/product-7.4c6ab223.PNG"
+  },
+  {
+    "revision": "d390282f89018d8777d36210b7df6d7b",
+    "url": "/store-recording/static/media/product-8.d390282f.PNG"
+  }
+]);
